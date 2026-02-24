b7fa6c62c334f397239fd5b0c00808b0dcd5484c
Mike Moffit <supermikemoffit@gmail.com>
2026-02-24 16:13:15 -0500

    Auto update Tue 02/24/2026 16:13:15.27

---
+++ assets/js/admin-media.js
@@ -0,0 +1,43 @@
+jQuery(function ($) {
+  $(document).on('click', '.panam-media-upload', function (e) {
+    e.preventDefault();
+
+    const $wrap = $(this).closest('.panam-media-field');
+    const $hidden = $wrap.find('.panam-media-id');
+    const $remove = $wrap.find('.panam-media-remove');
+    const $preview = $wrap.parent().find('.panam-media-preview');
+
+    const frame = wp.media({
+      title: 'Select Site Logo',
+      button: { text: 'Use this logo' },
+      multiple: false
+    });
+
+    frame.on('select', function () {
+      const attachment = frame.state().get('selection').first().toJSON();
+      $hidden.val(attachment.id);
+      $remove.prop('disabled', false);
+
+      if (attachment.url) {
+        $preview.html(
+          '<div class="panam-media-preview-box">' +
+            '<img class="panam-media-preview-img" src="' + attachment.url + '" alt="">' +
+          '</div>'
+        );
+      } else {
+        $preview.empty();
+      }
+    });
+
+    frame.open();
+  });
+
+  $(document).on('click', '.panam-media-remove', function (e) {
+    e.preventDefault();
+
+    const $wrap = $(this).closest('.panam-media-field');
+    $wrap.find('.panam-media-id').val('');
+    $(this).prop('disabled', true);
+    $wrap.parent().find('.panam-media-preview').empty();
+  });
+});
