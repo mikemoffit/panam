8b14d8c83da433f20784eced42368c397208ef49
Mike Moffit <supermikemoffit@gmail.com>
2026-02-24 16:54:07 -0500

    Auto update Tue 02/24/2026 16:54:07.26

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -67,7 +67,7 @@ add_action('wp_enqueue_scripts', function () {
 
   wp_dequeue_style('global-styles');
   wp_dequeue_style('classic-theme-styles');
-
+  wp_dequeue_style( 'global-styles' ); 
   wp_dequeue_style('wp-block-library');
   wp_dequeue_style('wp-block-library-theme');
   wp_dequeue_style('wp-block-navigation');
