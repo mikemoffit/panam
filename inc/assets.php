a576d98ec53efcc83be7c7d828e89257ced47d06
Mike Moffit <supermikemoffit@gmail.com>
2026-02-24 17:09:03 -0500

    Auto update Tue 02/24/2026 17:09:03.24

--- inc/assets.php
+++ inc/assets.php
@@ -45,3 +45,18 @@ add_action('admin_enqueue_scripts', function ($hook) {
     true
   );
 });
+
+add_action('admin_enqueue_scripts', function ($hook) {
+
+  if ($hook !== 'appearance_page_panam-theme-settings') {
+    return;
+  }
+
+  wp_enqueue_style('wp-color-picker');
+  wp_enqueue_script('wp-color-picker');
+
+  wp_add_inline_script(
+    'wp-color-picker',
+    "jQuery(document).ready(function($){ $('.panam-color-field').wpColorPicker(); });"
+  );
+});
