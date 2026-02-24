ab6c1a898ad7df30ccb5cc264d3046087a2472e0
Mike Moffit <supermikemoffit@gmail.com>
2026-02-24 16:55:19 -0500

    Auto update Tue 02/24/2026 16:55:19.29

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) exit;
 /**
  * Panam: Cleanup WordPress front-end head/styles
  */
-
+add_filter( 'should_load_separate_core_block_assets', '__return_false' );
 /**
  * Disable Emoji scripts/styles (front + admin)
  */
