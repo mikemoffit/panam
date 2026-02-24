a323ab7a046f9f320e97077ae65c095122a8f7d3
Mike Moffit <supermikemoffit@gmail.com>
2026-02-24 17:04:25 -0500

    Auto update Tue 02/24/2026 17:04:25.22

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
     <?php while (have_posts()) : the_post(); ?>
       <article <?php post_class(); ?>>
         <h1><?php the_title(); ?></h1>
-        <div><?php the_content(); ?></div>
+        <div class="panam-prose"><?php the_content(); ?></div>
       </article>
     <?php endwhile; ?>
   </div>
